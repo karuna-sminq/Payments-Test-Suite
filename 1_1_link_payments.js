e20fdcabd09b4a1de4e0f0ce5d2161f4a3bbe294
Karuna Lingham <karuna.lingham@codepandora.com>
2016-12-06 14:38:45 +0530

    Added config file for payments, setup Payments module for tests

---
+++ 1_1_link_payments.js
@@ -0,0 +1,13 @@
+describe("Biz App: Link payments", function () {
+
+  var sleep_time = 3000;
+
+  it("Instamojo / Razorpay", function () {
+
+    browser.get("http://staging.business.sminq.com/login")
+
+    browser.sleep(sleep_time);
+
+  });
+
+});
---
+++ payment_conf.js
@@ -0,0 +1,37 @@
+// conf.js
+
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+exports.config = {
+
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 2500000
+    },
+    specs: [
+      '1_1_link_payments.js',
+      // '1_2_gateway_payments.js'
+    ],
+
+    /*=============================================>>>>>
+    = Spec files =
+    ===============================================>>>>>*/
+
+    // '1_1_link_payments.js',
+    // '1_2_gateway_payments.js'
+
+    /*= End of Spec files =*/
+    /*=============================================<<<<<*/
+
+    onPrepare: function() {
+        browser.manage().window().maximize();
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: './reports/',
+                takeScreenshotsOnlyOnFailures: true,
+            })
+        );
+    }
+
+}
